abc3519f468312c325ecdb9c8da847f85961c0ed
monalloyd <monapluslloyd@gmail.com>
2023-09-08 11:46:26 +0200

    Update database manager and databaseLog record

--- src/main/java/com/codecool/marsexploration/mapexplorer/repository/log/DatabaseLog.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/repository/log/DatabaseLog.java
@@ -2,5 +2,5 @@ package com.codecool.marsexploration.mapexplorer.repository.log;
 
 import java.util.Map;
 
-public record DatabaseLog(int numberOfSteps, Map<String, Integer> resources, boolean successfulOutcome) {
+public record DatabaseLog(String timeStamp, int numberOfSteps, Map<String, Integer> resources, boolean successfulOutcome) {
 }
--- src/main/java/com/codecool/marsexploration/mapexplorer/repository/manager/DBConnectionManager.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/repository/manager/DBConnectionManager.java
@@ -3,6 +3,7 @@ package com.codecool.marsexploration.mapexplorer.repository.manager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnectionManager {
     private static final String JDBC_URL = "jdbc:sqlite:src/main/resources/dbs/MarsExploration.db";
@@ -11,6 +12,23 @@ public class DBConnectionManager {
     public DBConnectionManager() {
         try {
             connection = DriverManager.getConnection(JDBC_URL);
+            createTableIfNotExists();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createTableIfNotExists() {
+        //there is no boolean value in sqlite, use int 0 or 1
+        //BLOB is a catch-all that returns data as it was entered - we need to figure out how to log resources first, before defining this field
+        try (Statement statement = connection.createStatement()) {
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS logs (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "timestamp TEXT" +
+                    "number_of_steps INTEGER," +
+                    "resources BLOB" +
+                    "successful_outcome INTEGER)";
+            statement.executeUpdate(createTableSQL);
         } catch (SQLException e) {
             e.printStackTrace();
         }
