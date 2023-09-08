1c461d77811b51dd119a2aec15a1badc44723174
monalloyd <monapluslloyd@gmail.com>
2023-09-08 10:43:09 +0200

    Add constructor to repo impl

--- src/main/java/com/codecool/marsexploration/mapexplorer/repository/MarsExplorationRepository.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/repository/MarsExplorationRepository.java
@@ -1,10 +1,17 @@
 package com.codecool.marsexploration.mapexplorer.repository;
 
 import com.codecool.marsexploration.mapexplorer.repository.log.DatabaseLog;
+import com.codecool.marsexploration.mapexplorer.repository.manager.DBConnectionManager;
 
 import java.util.List;
 
 public class MarsExplorationRepository implements Repository<DatabaseLog> {
+    private final DBConnectionManager dbConnectionManager;
+
+    public MarsExplorationRepository(DBConnectionManager dbConnectionManager) {
+        this.dbConnectionManager = dbConnectionManager;
+    }
+
     @Override
     public DatabaseLog getById(int id) {
         return null;
