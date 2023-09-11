e3136105cf1de557753671d1e4ae3156edff7167
Christopher Bucher <bucher.christopher@gmail.com>
2023-09-11 10:22:14 +0200

    implemented console and filelogger

--- src/main/java/com/codecool/marsexploration/mapexplorer/logger/ConsoleLogger.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/logger/ConsoleLogger.java
@@ -3,6 +3,7 @@ package com.codecool.marsexploration.mapexplorer.logger;
 public class ConsoleLogger implements Logger{
     @Override
     public void log(String message) {
-
+        //TODO maybe instert timestamp
+        System.out.println("Log: " + message);
     }
 }
