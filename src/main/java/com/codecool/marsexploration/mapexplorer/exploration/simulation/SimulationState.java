2b3bd55b4d6d3d6cd00f2c6250c0dc5deab8358f
monalloyd <monapluslloyd@gmail.com>
2023-09-12 11:50:38 +0200

    Write simulation state class

--- src/main/java/com/codecool/marsexploration/mapexplorer/exploration/simulation/SimulationState.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/exploration/simulation/SimulationState.java
@@ -1,12 +1,53 @@
 package com.codecool.marsexploration.mapexplorer.exploration.simulation;
 
+import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
+import com.codecool.marsexploration.mapexplorer.maploader.model.Map;
+import com.codecool.marsexploration.mapexplorer.rover.Rover;
+
+import java.util.List;
+
 public class SimulationState {
-    //stores data:
-    //The number of steps -> I suppose this is the number of steps already taken?
-    //The number of steps required to reach a timeout
-    //The rover object
-    //The location of the spaceship
-    //The map
-    //The symbols of resources to monitor
-    //The exploration outcome
+    private int numberOfStepsTaken;
+    private boolean isRunning;
+    private final int simulationTimeout;
+    private final Rover rover;
+    private final Map map;
+    private final Coordinate shipCoordinates;
+    private final List<String> resourcesToScan;
+    private boolean success;
+
+    public SimulationState(int simulationTimeout, Rover rover, Map map, Coordinate shipCoordinates, List<String> resourcesToScan) {
+        this.simulationTimeout = simulationTimeout;
+        this.rover = rover;
+        this.map = map;
+        this.shipCoordinates = shipCoordinates;
+        this.resourcesToScan = resourcesToScan;
+        this.numberOfStepsTaken = 0;
+        this.isRunning = true;
+        this.success = false;
+    }
+
+    public void setIsRunningToFalse(){
+        isRunning = false;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setSuccessToTrue() {
+        success = true;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setNumberOfStepsTaken(int numberOfStepsTaken) {
+        this.numberOfStepsTaken = numberOfStepsTaken;
+    }
+
+    public int getNumberOfStepsTaken() {
+        return numberOfStepsTaken;
+    }
 }
