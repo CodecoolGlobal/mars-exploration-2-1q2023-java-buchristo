f1bb6f50acf4fb2b075bb08748e06f52997dc46f
monalloyd <monapluslloyd@gmail.com>
2023-09-12 10:37:12 +0200

    Create intermediate config file

--- src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
+++ src/main/java/com/codecool/marsexploration/mapexplorer/Application.java
@@ -1,22 +1,24 @@
 package com.codecool.marsexploration.mapexplorer;
 
-import com.codecool.marsexploration.mapexplorer.maploader.MapLoader;
-import com.codecool.marsexploration.mapexplorer.maploader.MapLoaderImpl;
+import com.codecool.marsexploration.mapexplorer.configuration.Config;
 import com.codecool.marsexploration.mapexplorer.maploader.model.Coordinate;
-import com.codecool.marsexploration.mapexplorer.rover.Rover;
-import com.codecool.marsexploration.mapexplorer.rover.RoverImpl;
 
 import java.util.List;
 
+import static com.codecool.marsexploration.mapexplorer.Constants.*;
+
 public class Application {
     private static final String workDir = "src/main";
+    static String mapFilePath = workDir + "/resources/maps/exploration-0.map";
+    static Coordinate landingCoordinates = new Coordinate(6, 6);
+    static List<String> resourcesToScanFor = List.of(MINERAL, WATER);
+    static int simTimeOut = 1000;
+    static Config config = new Config(mapFilePath, landingCoordinates, resourcesToScanFor, simTimeOut);
 
     public static void main(String[] args) {
-        String mapFilePath = workDir + "/resources/maps/exploration-0.map";
-        Coordinate landingCoordinates = new Coordinate(6, 6);
-
-        MapLoader mapLoader = new MapLoaderImpl();
-        mapLoader.load(mapFilePath);
+        //liste an resources
+        //visibility range
+        //
 
         // Create config, map loader, validators, analyzers etc.
 
